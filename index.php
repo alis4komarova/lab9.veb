a102d960c9ea4aad6f2d83cf94616a4ecf5ef4c7
alis4komarova <alis4komarova@mail.ru>
2025-12-25 10:12:02 +0300

    add different

--- index.php
+++ index.php
@@ -42,6 +42,14 @@
 
         if( $type == 'B' )         // если тип верстки В
             echo '<ul>';           // начинаем список
+        if( $type == 'C' ) 
+            echo '<ol>';
+        if( $type == 'D' ) {
+            echo '<table style="border-collapse: collapse; border: 1px solid black;">';
+            echo '<tr><th style="border: 1px solid black;">№</th><th style="border: 1px solid black;">x</th><th style="border: 1px solid black;">f(x)</th></tr>';
+        }
+        if( $type == 'E' ) 
+            echo '<div style="overflow: hidden;">';
 
         // цикл с заданным количеством итераций
         for( $i=0; $i < $encounting; $i++, $x+=$step )
@@ -51,17 +59,21 @@
             {
                 if( $x == 0 )      // если аргумент равен 0 (деление на ноль)
                     $f = 'error';  // не вычисляем функцию
-                else               // иначе
+                else{              // иначе
                     $f = (10 + $x) / $x;  // вычисляем функцию: (10+x)/x
+                    $f = round($f, 3);
+                }
             }
             else                   // иначе
             if( $x < 20 )          // если аргумент меньше 20
             {
                 $f = $x / 7 * ($x - 2);   // вычисляем функцию: x/7*(x-2)
+                $f = round($f, 3);
             }
             else                   // иначе (x ≥ 20)
             {
                 $f = $x * 8 + 2;   // вычисляем функцию: x*8+2
+                $f = round($f, 3);
             }
             
             // проверка на выход за пределы диапазона (если значение не 'error')
@@ -72,17 +84,33 @@
             }
             
             // вывод в зависимости от типа верстки
-            if( $type == 'A' )     // если тип верстки А
-            {
-                echo 'f('.$x.')='.$f;     // выводим аргумент и значение функции
-                if( $i < $encounting-1 )  // если это не последняя итерация цикла
-                    echo '<br>';          // выводим знак перевода строки
-            }
-            else                   // иначе
-            if( $type == 'B' )     // если тип верстки В
-            {
-                // выводим данные как пункт списка
-                echo '<li>f('. $x.')='.$f.'</li>';
+            switch ($type) {
+                case 'A': // простая верстка текстом
+                    echo 'f(' . $x . ')=' . $f;
+                    if ($i < $encounting - 1) { // если это не последняя итерация цикла
+                        echo '<br>';            // выводим знак перевода строки
+                    }
+                    break;
+
+                case 'B': // маркированный список
+                case 'C': //нумерованный список
+                    echo '<li>f(' . $x . ')=' . $f . '</li>';
+                    break;
+
+                case 'D': // табличная верстка
+                    $row_num = $i + 1;
+                    echo '<tr>';
+                    echo '<td style="border: 1px solid black; text-align: center;">' . $row_num . '</td>';
+                    echo '<td style="border: 1px solid black; text-align: center;">' . $x . '</td>';
+                    echo '<td style="border: 1px solid black; text-align: center;">' . $f . '</td>';
+                    echo '</tr>';
+                    break;
+
+                case 'E': // блочная верстка
+                    echo '<div style="float: left; border: 2px solid red; margin: 8px; padding: 5px;">';
+                    echo 'f(' . $x . ')=' . $f;
+                    echo '</div>';
+                    break;
             }
             
             // сбор статистики (только для числовых значений)
@@ -95,15 +123,28 @@
             }
         }
 
-        if( $type == 'B' )         // если тип верстки В
-            echo '</ul>';          // закрываем тег списка
+        // завершение вывода в зависимости от типа верстки
+        switch ($type) {
+            case 'B': // маркированный список
+                echo '</ul>';
+                break;
+            case 'C': // нумерованный список
+                echo '</ol>';
+                break;
+            case 'D': // табличная верстка
+                echo '</table>';
+                break;
+            case 'E': // блочная верстка
+                echo '</div>'; // закрываем контейнер для обтекания
+                break;
+        }
 
         // вывод статистики
         echo '<br><br>Статистика:<br>';
         echo 'Максимальное значение функции: '.$max_f.'<br>';
         echo 'Минимальное значение функции: '.$min_f.'<br>';
-        echo 'Среднее арифметическое: '.($count > 0 ? $sum/$count : 0).'<br>';
-        echo 'Сумма значений: '.$sum.'<br>';
+        echo 'Среднее арифметическое: '.($count > 0 ? round($sum/$count, 3) : 0).'<br>';
+        echo 'Сумма значений: '.round($sum, 3).'<br>';
         ?>
     </main>
 
